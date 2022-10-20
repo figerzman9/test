e8230a62b7855fb140668aebc5d1909901c216b4
figerzman <figerzman@naver.com>
2022-10-20 22:20:15 +0900

    test

--- src/main/java/com/test/test/HomeController.java
+++ src/main/java/com/test/test/HomeController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Handles requests for the application home page.
-11 */
+12 */
 @Controller
 public class HomeController {
 	
